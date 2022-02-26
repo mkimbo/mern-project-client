a303253d846b8f17973e308efdb910a1ffc8a9b7
[jack mkimbo] <[nickihllj@gmail.com]>
2022-02-27 01:28:13 +0300

    added hosted backend config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3001/api";
+//const BASE_URL = "http://localhost:3001/api";
+const BASE_URL = process.env.BACKEND_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
