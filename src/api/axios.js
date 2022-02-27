817a97994cdd7e5c024b6119a9397a12bf6b21b4
[jack mkimbo] <[nickihllj@gmail.com]>
2022-02-27 10:38:19 +0300

    fixed backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //const BASE_URL = "http://localhost:3001/api";
-const BASE_URL = process.env.BACKEND_URL;
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
