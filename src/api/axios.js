4f02cb7fef54d857d3fe09dea64173187bf7635c
Jack <jackmkimbo@gmail.com>
2023-07-14 16:14:24 +0300

    changed backed deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL =
-  process.env.REACT_APP_BACKEND_URL || "http://localhost:3001/api";
+const BASE_URL = "https://mern-project-server-5688.onrender.com";
+//  process.env.REACT_APP_BACKEND_URL || "http://localhost:3001/api";
 
 export default axios.create({
   baseURL: BASE_URL,
